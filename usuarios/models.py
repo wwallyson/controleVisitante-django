6af10f03dd46f48bf159906cc56110de01ef757b
samuelamorimm <samuelhenriqueamorim@gmail.com>
2024-09-26 15:06:12 -0300

    feat: criação do projeto e app usuarios

---
+++ usuarios/models.py
@@ -0,0 +1,54 @@
+from django.db import models
+from django.contrib.auth.models import(AbstractBaseUser, PermissionsMixin, BaseUserManager)
+
+class UsuarioManager(BaseUserManager):
+    def create_user(self, email, password=None):
+        usuario = self.model(
+           email = self.normalize_email(email)
+       )
+        usuario.is_active = True
+        usuario.is_staff = False
+        usuario.is_superuser = False
+       
+        if password:
+            usuario.set_password(password)
+       
+        usuario.save()
+        return usuario    
+   
+    def create_superuser(self, email, password):
+        usuario = self.model(
+            email = self.normalize_email(email),
+            password = password,
+        )
+        usuario.is_active = True
+        usuario.is_staff = True
+        usuario.is_superuser = True
+                
+        if password:
+            usuario.set_password(password)
+        
+        usuario.save()
+        return usuario    
+       
+
+class Usuario(AbstractBaseUser, PermissionsMixin):
+    email = models.EmailField(verbose_name='e-mail do usuario',max_length=200,unique=True)
+    is_active = models.BooleanField(verbose_name='usuarioesta ativo', default=True)
+    is_staff = models.BooleanField(verbose_name='usuário e da equipe de desenvolvimento', default=False)
+    is_superuser = models.BooleanField(verbose_name='Usuario e um superuario', default=False)
+    
+    USERNAME_FIELD = 'email' 
+    objects = UsuarioManager()
+       
+
+    class Meta:
+        verbose_name = 'Usuário'
+        verbose_name_plural = 'Usurios'
+        db_table = 'usuario'
+
+    def __str__(self):
+        return self.email
+    
+    
+    
