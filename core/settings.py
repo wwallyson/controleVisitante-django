2ee69f0b83ee2dc15aa7525d09fb0d13e5e45d60
samuelamorimm <samuelhenriqueamorim@gmail.com>
2024-09-27 15:21:21 -0300

    feat: criação do app dasj board

--- core/settings.py
+++ core/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
 ]
 INSTALLED_APPS += [
     'usuarios',
+    'porteiros',
 ]
 
 MIDDLEWARE = [
---
+++ porteiros/admin.py
@@ -0,0 +1,4 @@
+from django.contrib import admin
+from .models import Porteiro
+# Register your models here.
+admin.site.register(Porteiro)
