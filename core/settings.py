9885a99602a8259ece3d4089f159e5fad5ed6d58
samuelamorimm <samuelhenriqueamorim@gmail.com>
2024-10-02 17:33:01 -0300

    hoje02/10

--- core/settings.py
+++ core/settings.py
@@ -37,7 +37,13 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    
 ]
+INSTALLED_APPS+=[
+    'widget_tweaks'
+]
+
+
 INSTALLED_APPS += [
     'usuarios',
     'porteiros',
@@ -132,3 +138,10 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+LOGIN_URL= 'login'
+LOGIN_REDIRECT_URL = 'index'
+
+DEFAULT_AUTO_FIELD = 'django.db.models.BiAutoField'
+
